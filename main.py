9950e3e1d73aefb5b6a5f8440a81ec0cfb9a3e30
Dmitry Rogachko <dmitryrogachko@gmail.com>
2018-04-11 22:28:58 -0700

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import slackbot
-import time
+import time	
 import os
+import requests
+import json
+
 
 def handle_command(slack_api, command, channel):
-	
-	EXAMPLE_COMMAND = 'do'
-	if command.lower().startswith('hi') or command.lower().startswith('hey') or command.lower().startswith('hello'):
-    		slack_api.rtm_send_message(channel, 'Hey, I\'m your slack bot, how may I help you?')
-	else:
-		slack_api.rtm_send_message(channel, 'Invalid Command: Not Understood')
+    	
+		if command.lower().startswith('hi') or command.lower().startswith('hey') or command.lower().startswith('hello'):
+    			slack_api.rtm_send_message(channel, 'Hi, I\'m your weather bot. Would you like to see the current weather in Vancouver? Type "weather" ')
+		elif command.lower().startswith('weather'):
+    			weather_conditions = get_weather()
+    			for i in weather_conditions:
+    					slack_api.rtm_send_message(channel, '\n'+str(i)+' : '+str(weather_conditions[i]))
+		else:
+				slack_api.rtm_send_message(channel, 'Sorry, I don\'t understand this')			
+				
+    				
+def get_weather():
+	response = requests.get('http://api.openweathermap.org/data/2.5/weather?id=6173331&appid=7d82f7ba43beaf7ee5c79bca20718fba')	
+	json_object = response.json()
+	temp = float(json_object['main']['temp'])
+	temp_celius = int(temp - 273.15)
+	humidity = int(json_object['main']['humidity'])
+	wind_speed = float(json_object['wind']['speed'])
+	weather = json_object['weather'][0]['main']
+	weather_dictionary = dict({'temperature': temp_celius, 'humidity': humidity, 'wind': wind_speed, 'precipitation': weather})
+	return weather_dictionary
 	
 def main():
-	
+    	
 	READ_WEBSOCKET_DELAY = 1 
 	slack_api = slackbot.connect()
 	botname = os.environ.get("BOTNAME")
